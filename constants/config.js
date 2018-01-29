69bcb533eb1dce997591e40582adca4420ee4b5e
Gavin <gavin199502@gmail.com>
2018-01-29 08:32:30 +0800

    chore(constants/config): remove useless configuration.

--- constants/config.js
+++ constants/config.js
@@ -1,6 +1,5 @@
 export const server = {
-  baseUrl: 'https://www.easy-mock.com/mock/5a3a31f9ccd95b5cf43c5d91/api/',
-  httpMock: 'cmsapi.tianpeng.com'
+  baseUrl: 'https://www.easy-mock.com/mock/5a3a31f9ccd95b5cf43c5d91/api/'
 };
 
 export const other = {
